ae69c2ff684425966a4bcef18749b7722606e992
RamyaRamathas15 <71037877+RamyaRamathas15@users.noreply.github.com>
2021-03-04 17:45:09 -0400

    navigation-page modified

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,93 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonIcon, IonImg, IonItem, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import { pin, wifi, wine, warning, walk } from 'ionicons/icons';
+import test from './test.png';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonContent>
+      <IonCard>
+     
+        
+     <IonCardHeader>
+     
+       <IonCardSubtitle>Event</IonCardSubtitle>
+       {/* <IonCardTitle>Card Title</IonCardTitle> */}
+
+     </IonCardHeader>
+     <IonItem href="#" className="ion-activated">
+     <IonImg src={test}/>
+     </IonItem>
+     <IonCardContent>
+      Allows to book event for your loved ones
+     </IonCardContent>
+    
+   </IonCard>
+
+        {/* <IonCard>
+          <IonItem>
+            <IonIcon slot="start" />
+            <IonLabel>ion-item in a card, icon left, button right</IonLabel>
+            <IonButton fill="outline" slot="end">View</IonButton>
+          </IonItem>
+
+          <IonCardContent>
+            This is content, without any paragraph or header tags,
+            within an ion-cardContent element.
+      </IonCardContent>
+        </IonCard> */}
+
+      <IonCard>
+     
+        
+        <IonCardHeader>
+        
+          <IonCardSubtitle>Event</IonCardSubtitle>
+          {/* <IonCardTitle>Card Title</IonCardTitle> */}
+
+        </IonCardHeader>
+        <IonItem href="#" className="ion-activated">
+        <IonImg src={test}/>
+        </IonItem>
+        <IonCardContent>
+         Allows to book event for your loved ones
+        </IonCardContent>
+       
+      </IonCard>
+
+        <IonCard>
+          <IonItem href="#" className="ion-activated">
+            <IonIcon icon={wifi} slot="start" />
+            <IonLabel>Card Link Item 1 activated</IonLabel>
+          </IonItem>
+          </IonCard>
+          <IonCard>
+
+          <IonItem href="#">
+            <IonIcon icon={wine} slot="start" />
+            <IonLabel>Card Link Item 2</IonLabel>
+          </IonItem>
+          </IonCard>
+          <IonCard>
+          <IonItem className="ion-activated">
+            <IonIcon icon={warning} slot="start" />
+            <IonLabel>Card Button Item 1 activated</IonLabel>
+          </IonItem>
+          </IonCard>
+          <IonCard>
+          <IonItem>
+            <IonIcon icon={walk} slot="start" />
+            <IonLabel>Card Button Item 2</IonLabel>
+          </IonItem>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
